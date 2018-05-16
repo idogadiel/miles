f8ec01c0b1fbe080db09232bdd9b674966cdfe20
daniel <daniel.gilon@gmail.com>
2018-05-16 19:27:28 +0300

    UI integration started

--- src/main/resources/static/UI/js/index.js
+++ src/main/resources/static/UI/js/index.js
@@ -3,7 +3,7 @@ var scotchApp = angular.module('scotchApp', ['ngRoute']);
 scotchApp.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'home.html',
+            templateUrl: '_home.html',
             controller: 'mainController'
         })
 
