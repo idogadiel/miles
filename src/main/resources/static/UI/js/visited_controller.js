03d4612608c11ac09a06c96a75e7ac664386a57a
daniel <daniel.gilon@gmail.com>
2018-05-27 20:26:33 +0300

    Created http wrapper

--- src/main/resources/static/UI/js/visited_controller.js
+++ src/main/resources/static/UI/js/visited_controller.js
@@ -1,56 +1,56 @@
-scotchApp.controller('visitedController', function ($scope, $rootScope, $http, UserService) {
+scotchApp.controller('visitedController', function ($scope, $rootScope, $http, UserService, serverHttp) {
 
     UserService.getUser()
 
-    // get all destinations
-    $http({
-        method: 'GET',
-        withCredentials: true,
-        url: 'http://127.0.0.1:8080/takenflight/getAllTakenFlights'
-    }).then(function successCallback(response) {
-        $scope.allTakenFlights = response.data;
-        console.log($scope.allTakenFlights)
-    }, function errorCallback(response) {
-        console.log(response);
-        $scope.allTakenFlights = "error";
-    });
+    serverHttp.GET("takenflight/getAllTakenFlights",{}).then(function(data){
+        $scope.allTakenFlights = data;
+        console.log("getAllTakenFlights",data);
+    })
 
 //    // get recommendations
-    $http({
-        method: 'GET',
-        withCredentials: true,
-        url: 'http://127.0.0.1:8080/recommender/recommend'
-    }).then(function successCallback(response) {
-        console.log("recommend","successCallback",response);
-        $scope.mostBeneficialAirline = response.data.mostBeneficialAirline;
-    }, function errorCallback(response) {
-        console.log("recommend","errorCallback",response);
-    });
+    serverHttp.GET("recommender/recommend",{}).then(function(data){
+        console.log("recommend","successCallback",data);
+        $scope.mostBeneficialAirline = data.mostBeneficialAirline;
+    })
+
+    $scope.from = "TLV"
+    $scope.to = "LAX"
+    $scope.flightNumber = "EZY643"
+    $scope.ticketNumber = "ABC123"
+    $scope.nameOnTicket = "Daniel"
+    $scope.dateOfFlight = "01012010"
+    $scope.seatType = "A"
+    var jsonObj =
+        {
+            "from": $scope.from,
+            "to": $scope.to,
+            "flightNumber": $scope.flightNumber,
+            "ticketNumber": $scope.ticketNumber,
+            "nameOnTicket": $scope.nameOnTicket,
+            "dateOfFlight": $scope.dateOfFlight,
+            "seatType": $scope.seatType
+        };
+    console.log("stringified JSON: " ,JSON.stringify(jsonObj))
 
     // add destination func :
     $scope.addDestination = function () {
-            $scope.message = "";
-            var jsonObj =
-            {
-                "from": $scope.from,
-                "to": $scope.to,
-                "flightNumber": $scope.flightNumber,
-                "ticketNumber": $scope.ticketNumber,
-                "nameOnTicket": $scope.nameOnTicket,
-                "dateOfFlight": $scope.dateOfFlight,
-                "seatType": $scope.seatType
-            };
-            $http({
-                url: "http://127.0.0.1:8080/takenflight/addTakenFlight/",
-                method: "POST",
-                withCredentials: true,
-                data: jsonObj
-            }).success(function (data, status, headers, config) {
-                $scope.showLoader = false;
-                $scope.allTakenFlights.push(jsonObj)
-            }).error(function (data, status, headers, config) {
-               // alert("c");
-            });
-
+        $scope.message = "";
+        var jsonObj =
+        {
+            "from": $scope.from,
+            "to": $scope.to,
+            "flightNumber": $scope.flightNumber,
+            "ticketNumber": $scope.ticketNumber,
+            "nameOnTicket": $scope.nameOnTicket,
+            "dateOfFlight": $scope.dateOfFlight,
+            "seatType": $scope.seatType
         };
+
+        serverHttp.POST("takenflight/addTakenFlight/", jsonObj ).then(function(data){
+            $scope.showLoader = false;
+            $scope.allTakenFlights.push(jsonObj)
+            console.log("addTakenFlight",data);
+        });
+    };
+
 });
